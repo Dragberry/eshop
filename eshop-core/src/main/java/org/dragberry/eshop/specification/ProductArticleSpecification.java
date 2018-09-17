35f6b59f4cb830493dcd9147be5de5b145811449
Maksim Dragun <maksimdragun@live.com>
2018-09-18 00:20:29 +0300

    product attributes search

--- eshop-core/src/main/java/org/dragberry/eshop/specification/ProductArticleSpecification.java
+++ eshop-core/src/main/java/org/dragberry/eshop/specification/ProductArticleSpecification.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -28,12 +27,17 @@ public class ProductArticleSpecification implements Specification<ProductArticle
     
     private static final Pattern OPTION_PATTERN = Pattern.compile("option\\[(.*?)\\]$");
 
+    private static final Pattern ATTRIBUTE_PATTERN = Pattern.compile("attribute\\[(.*?)\\]$");
+    
     private final String categoryReference;
     
     private final Map<String, String[]> searchParams;
     
+    private Join<?, ?> categoryRoot;
     
+    private Join<?, ?> productRoot;
     
+    private Join<?, ?> optionRoot;
     
     public ProductArticleSpecification(String categoryReference, Map<String, String[]> searchParams) {
         this.categoryReference = categoryReference;
@@ -42,17 +46,9 @@ public class ProductArticleSpecification implements Specification<ProductArticle
     
     @Override
     public Predicate toPredicate(Root<ProductArticle> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-        var productRoot = root.join("products");
-        var categoryRoot = root.join("categories");
-        Join<?, ?> optionRoot = null;
-        Join<?, ?> attributesRoot = null;
-        
-        Subquery<ProductAttributeBoolean> sqPAB = query.subquery(ProductAttributeBoolean.class);
-        Root<ProductAttributeBoolean> fromPAB = sqPAB.from(ProductAttributeBoolean.class);
-        sqPAB.select(fromPAB.get("productArticle").get("entityKey"));
-        List<Predicate> wherePAB = new ArrayList<>();
-        
-        List<Predicate> where = new ArrayList<>();
+    	categoryRoot = root.join("categories");
+    	productRoot = root.join("products");
+    	List<Predicate> where = new ArrayList<>();
         if (StringUtils.isNotBlank(categoryReference)) {
             where.add(cb.equal(categoryRoot.get("reference"), categoryReference));
         }
@@ -79,41 +75,58 @@ public class ProductArticleSpecification implements Specification<ProductArticle
                 } catch (Exception exc) {}
                 continue;
             }
-            Matcher optionMatcher = OPTION_PATTERN.matcher(name);
-			if (optionMatcher.find() && values.length > 0) {
-				if (optionRoot == null) {
-					optionRoot = productRoot.join("options");
-				}
-            	where.add(cb.equal(optionRoot.get("name"), optionMatcher.group(1)));
-            	where.add(optionRoot.get("value").in(Arrays.asList(values)));
+            
+            Matcher optionMatcher;
+            if ((optionMatcher = OPTION_PATTERN.matcher(name)).find()) {
+            	where.addAll(getOptionExpressions(optionMatcher.group(1), values, root, query, cb));
             	continue;
-			}
-			
-			Pattern attrPattern = Pattern.compile("attribute\\[(.*?)\\]$");
-			Matcher attrMatcher = attrPattern.matcher(name);
-			if (attrMatcher.find() && values.length > 0) {
-    			Predicate attrPredicate = cb.equal(fromPAB.get("name"), attrMatcher.group(1));
-    			List<Predicate> orList = new ArrayList<>();
-    			for (String value : values) {
-    			    if (Boolean.FALSE.toString().equalsIgnoreCase(value) || (Boolean.TRUE.toString().equalsIgnoreCase(value))) {
-    			        orList.add(cb.equal(fromPAB.get("value"), Boolean.parseBoolean(value)));
-    			    } else {
-    			        orList.add(cb.and(cb.equal(fromPAB.get("value"), true), cb.and(cb.equal(fromPAB.get("description"), value))));
-    			    }
-    			}
-    			wherePAB.add(cb.and(attrPredicate, cb.or(orList.toArray(new Predicate[orList.size()]))));
-			}
-        }
-        
-        if (!wherePAB.isEmpty()) {
-            if (attributesRoot == null) {
-                attributesRoot = productRoot.join("attributes");
             }
-            where.add(attributesRoot.in(
-                    sqPAB.select(fromPAB).where(wherePAB.toArray(new Predicate[wherePAB.size()]))));
+            Matcher attrMatcher;
+            if ((attrMatcher = ATTRIBUTE_PATTERN.matcher(name)).find()) {
+            	where.addAll(getAttributeExpressions(attrMatcher.group(1), values, root, query, cb));
+            	continue;
+            }
         }
         query.distinct(true);
         return cb.and(where.toArray(new Predicate[where.size()]));
+    }
+    
+    private List<Predicate> getOptionExpressions(String optionName, String[] values, Root<ProductArticle> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+		if (values.length > 0) {
+			if (optionRoot == null) {
+				optionRoot = productRoot.join("options");
+			}
+			return List.of(
+					cb.equal(optionRoot.get("name"), optionName),
+					optionRoot.get("value").in(Arrays.asList(values)));
+		}
+		return List.of();
+    }
+    
+    private List<Predicate> getAttributeExpressions(String attributeName, String[] values, Root<ProductArticle> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+		if (values.length > 0) {
+			List<Predicate> predicates = new ArrayList<>(values.length);
+			for (String value : values) {
+			    if (Boolean.FALSE.toString().equalsIgnoreCase(value) || (Boolean.TRUE.toString().equalsIgnoreCase(value))) {
+			    	Subquery<Long> sqPAB = query.subquery(Long.class);
+			        Root<ProductAttributeBoolean> fromPAB = sqPAB.from(ProductAttributeBoolean.class);
+			        predicates.add(cb.exists(sqPAB.select(fromPAB.get("entityKey")).where(
+			        		cb.equal(root, fromPAB.get("productArticle")),
+			        		cb.equal(fromPAB.get("name"), attributeName),
+			        		cb.equal(fromPAB.get("value"), Boolean.parseBoolean(value)))));
+			    } else {
+			    	Subquery<Long> sqPAB = query.subquery(Long.class);
+			        Root<ProductAttributeBoolean> fromPAB = sqPAB.from(ProductAttributeBoolean.class);
+			        predicates.add(cb.exists(sqPAB.select(fromPAB.get("entityKey")).where(
+			        		cb.equal(root, fromPAB.get("productArticle")),
+			        		cb.equal(fromPAB.get("name"), attributeName),
+			        		cb.equal(fromPAB.get("value"), true),
+			        		cb.and(cb.equal(fromPAB.get("description"), value)))));
+			    }
+			}
+			return predicates;
+		}
+    	return List.of();
     }
 
 }
