522a515c6d184b3c2a7dc285d519efc78f34188e
Maksim Dragun <maksimdragun@live.com>
2018-10-02 01:02:35 +0300

    Add fileter bean

--- eshop-client/src/main/java/org/dragberry/eshop/application/WebConfig.java
+++ eshop-client/src/main/java/org/dragberry/eshop/application/WebConfig.java
@@ -1,5 +1,6 @@
 package org.dragberry.eshop.application;
 
+import org.dragberry.eshop.filter.AuditLogFilter;
 import org.dragberry.eshop.interceptor.AppInfoInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,11 @@ public class WebConfig {
     @Bean
     public AppInfoInterceptor appInfoInterceptor() {
         return new AppInfoInterceptor();
+    }
+    
+    @Bean
+    public AuditLogFilter auditLogFilter() {
+        return new AuditLogFilter();
     }
     
 }
