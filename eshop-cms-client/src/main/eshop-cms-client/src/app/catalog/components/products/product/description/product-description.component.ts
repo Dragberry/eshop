d11d7af98344b07e8f743550a6624c364bb0ba5e
Maksim Dragun <maksimdragun@live.com>
2018-11-24 01:58:40 +0300

    product details cms

--- eshop-cms-client/src/main/eshop-cms-client/src/app/catalog/components/products/product/description/product-description.component.ts
+++ eshop-cms-client/src/main/eshop-cms-client/src/app/catalog/components/products/product/description/product-description.component.ts
@@ -3,7 +3,10 @@ import { ProductArticleDetails } from 'src/app/catalog/model/product-article-det
 
 @Component({
     selector: 'app-product-description',
-    templateUrl: './product-description.component.html'
+    templateUrl: './product-description.component.html',
+    styles: [
+      '.product-description-full {max-height: 50rem; overflow: scroll}'
+    ]
 })
 export class ProductDescriptionComponent {
 
